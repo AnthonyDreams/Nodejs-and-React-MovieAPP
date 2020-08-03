349975041f502d8cbaa5b86fa0cc1ce85973754c
Anthony Taveras <theanthony2d@gmail.com>
2020-08-03 00:43:55 -0400

    first commit

--- waomovies_api/src/app/Controllers/httpErrors/actorNotFound.ts
+++
@@ -1,7 +0,0 @@
-import { HttpError } from "routing-controllers";
- 
-export class ActorNotFoundError extends HttpError {
-    constructor() {
-        super(404, "Actor not found!");
-    }
-}
--- waomovies_client/src/utils/Images/ToBase64.js
+++
@@ -1,9 +0,0 @@
-
-export default (image, cb) =>  {
-    var file = image;
-    var reader = new FileReader();
-    reader.onloadend = function() {
-        cb(reader.result.split(",")[1])
-    }
-    reader.readAsDataURL(file);
-  }
